9daf3fe7215880583577bd94e3197276d4cb2e90
Emilia Gimenez <emilia.gimenez@klarna.com>
2021-03-26 13:31:01 +0100

    cleanup create react app and basic page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import jumboData from "./fixtures/jumbo";
+import Jumbotron from "./components/jumbotron";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Jumbotron.Container>
+      {
+        jumboData.map(item => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
+            <Jumbotron.Image src={item.image} alt={item.alt}></Jumbotron.Image>
+          </Jumbotron>
+        ))
+      }
+    </Jumbotron.Container>
   );
 }
 
